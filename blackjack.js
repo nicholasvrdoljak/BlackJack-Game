6bdf3344ae46a30bdec9e0f95d8ad309c0dc3786
nicholasvrdoljak <admin@nicholasvrdoljak.com>
2018-02-26 20:35:35 -0800

    Updated win/lose logic.

--- blackjack.js
+++ blackjack.js
@@ -44,7 +44,7 @@ class Deck {
 
 //check your card values
 let checkValues = (cardArray) => {
-//add up card values in the given array's cards
+  //add up card values in the given array's cards
   let total = 0;
   let containsAce = false;
   let numOfAces = 0;
@@ -66,17 +66,51 @@ let checkValues = (cardArray) => {
       if (tempTotal > 21 && numOfAces > 0){
         tempTotal = total + 1;
         if (numOfAces - 1 === 0){
-          return tempTotal;
+          return [tempTotal, containsAce];
         } else {
-          return aces(containsAce, numOfAces - 1, tempTotal)
+          return aces(containsAce, numOfAces - 1, tempTotal);
         }
       } else {
-        return tempTotal;
+        return [tempTotal, containsAce];
       }
     } else {
-      return total;
+      return [total, containsAce];
     }
   }
   return aces(containsAce, numOfAces, total);
 }
+
+//check for the dealer to hit
+let dealerHitting = (dealerCards) => {
+  //hit dealer if necessary
+  let total = checkValues(dealerCards)[0];
+  let aces = checkValues(dealerCards)[1];
+    //<17, hit, update
+    if (total<17){
+      dealerCards.push(Deck.dealCards());
+      return dealerHitting(dealerCards);
+    //soft 17, hit, hard 17, hold
+    }else if (total === 17 && aces){
+      dealerCards.push(Deck.dealCards());
+      return dealerHitting(dealerCards);
+    }else if (total >= 17 && total <21){
+      checkWinner(yourCards, dealerCards);
+    }
+}
+
+let checkWinner = (yourCards, dealerCards) => {
+  let yTotal = checkValues(yourCards)[0];
+  let dTotal = checkValues(dealerCards)[0];
+  if (yTotal > dTotal){
+    //you win
+  }else if (dTotal > yTotal){
+    //you lose
+  }else {
+    //tie
+  }
+
+}
+//compare your cards and dealer cards
+// let whoWon = () => {
 //
+// }
